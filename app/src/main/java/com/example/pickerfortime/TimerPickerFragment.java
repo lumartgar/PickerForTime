71af2acb92f75b1864d4b368591aeed98c85c450
lumartgar <lumartgar@unirioja.es>
2021-03-09 15:18:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pickerfortime/TimerPickerFragment.java
@@ -0,0 +1,40 @@
+package com.example.pickerfortime;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TimerPickerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimerPickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker.
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processTimePickerResult(hour, minute);
+    }
+}
